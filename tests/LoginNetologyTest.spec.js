b4295b67ea166c9fc9f3b4d537c7329a18ffda6e
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-06-17 16:19:58 +0400

    Added invalid login test

--- tests/LoginNetologyTest.spec.js
+++ tests/LoginNetologyTest.spec.js
@@ -1,5 +1,11 @@
 const { test, expect } = require("@playwright/test");
-const { email, password, id } = require("../user");
+const {
+  email,
+  password,
+  id,
+  invalidEmail,
+  invalidPassword,
+} = require("../user");
 
 test("Success login", async ({ page }) => {
   // Go to https://netology.ru/
@@ -18,13 +24,37 @@ test("Success login", async ({ page }) => {
   await page.fill('[placeholder="Пароль"]', password);
 
   // Click [data-testid="login-submit-btn"]
-  await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://netology.ru/profile/8933209' }*/),
-    page.click('[data-testid="login-submit-btn"]'),
-  ]);
+  await page.getByTestId("login-submit-btn").click();
 
   await expect(page).toHaveURL(`https://netology.ru/profile/${id}`);
 
-  await expect(page.getByRole('heading', { name: 'Моё обучение' })).toBeVisible({ timeout: 20000 });
+  await expect(page.getByRole("heading", { name: "Моё обучение" })).toBeVisible(
+    { timeout: 20000 },
+  );
+});
 
+test("Login with invalid credentials", async ({ page }) => {
+  // Go to https://netology.ru/
+  await page.goto("https://netology.ru/");
+
+  // Click text=Войти
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'https://netology.ru/?modal=sign_in' }*/),
+    page.click("text=Войти"),
+  ]);
+
+  // Fill [placeholder="Email"]
+  await page.fill('[placeholder="Email"]', invalidEmail);
+
+  // Fill [placeholder="Пароль"]
+  await page.fill('[placeholder="Пароль"]', invalidPassword);
+
+  // Click [data-testid="login-submit-btn"]
+  await page.getByTestId("login-submit-btn").click();
+
+  await expect(page.getByTestId("login-error-hint")).toBeVisible();
+
+  await expect(page.getByTestId("login-error-hint")).toContainText(
+    "Вы ввели неправильно логин или пароль",
+  );
 });
